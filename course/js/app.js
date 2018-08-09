cad651a651f23416e8d31b66f662f8114b260368
Igor Ramos <igor.goncalves@sunsetcom.com.br>
2018-08-09 16:32:52 -0300

    inicio do projeto + pequena introdução ao vue.js

---
+++ course/js/app.js
@@ -0,0 +1,14 @@
+var hello = new Vue({
+    el:'#hello',
+    data: {
+        msg: "Hello Vue!",
+        peoples: [
+            {name: "Maria"},
+            {name: "Pedro"},
+            {name: "Gustavo"},
+            {name: "Ana"},
+            {name: "Gabriela"}
+            
+        ]
+    }
+});
